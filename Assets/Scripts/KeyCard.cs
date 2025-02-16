91b55575d2c9053cc254609b65847d5bceae6600
npnorman <npn.not@gmail.com>
2025-02-16 16:30:30 -0500

    Updated Keycard to have string and ID

--- Assets/Scripts/KeyCard.cs
+++ Assets/Scripts/KeyCard.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class KeyCard : MonoBehaviour
 {
+
+    public string cardText = "A";
+    public int cardID = 0;
+
+    //text
+    TMP_Text textObj;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        textObj = GetComponentInChildren<TMP_Text>();
+        textObj.text = cardText;
     }
 }
