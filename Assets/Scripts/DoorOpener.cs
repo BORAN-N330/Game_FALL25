bd9d05514274542d1d20120b984f47258ff0b59e
npnorman <npn.not@gmail.com>
2025-02-16 18:03:37 -0500

    Added text to doors

--- Assets/Scripts/DoorOpener.cs
+++ Assets/Scripts/DoorOpener.cs
@@ -1,6 +1,7 @@
 using System.Data.Common;
 using UnityEditor;
 using UnityEngine;
+using TMPro;
 
 public class DoorOpener : MonoBehaviour
 {
@@ -8,12 +9,20 @@ public class DoorOpener : MonoBehaviour
 
     GameManager gameManager;
 
+    //text on door
+    TMP_Text text;
+
+    public string doorText = "A";
     public int id = 0;
+    
     bool isOpen = false;
 
     private void Start() {
         animator = GetComponent<Animator>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        text = GetComponentInChildren<TMP_Text>();
+        text.text = doorText;
     }
 
     public void OpenDoor() {
