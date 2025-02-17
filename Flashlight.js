bdcce869de31c0c540b0ad62a6dd16a94f7790dd
Brett1217 <Beasleybrett682@gmail.com>
2025-02-17 02:34:50 -0500

    Create Flashlight.js

---
+++ Flashlight.js
@@ -0,0 +1,21 @@
+// JavaScript source code
+using UnityEngine;
+using System.Collections;
+
+public class FlashLight : MonoBehaviour {
+    private Light myLight;
+      
+      
+    void Start ()
+    {
+        myLight = GetComponent<Light>();
+    }
+      
+    void Update ()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+}
